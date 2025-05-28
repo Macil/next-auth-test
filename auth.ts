44a1784b6de8ba3ee317e415ecbc195db762a5b1
Chris Cowan <agentme49@gmail.com>
2025-05-27 20:21:49 -0700

    add next-auth trustHost setting to make next-auth work in `npm run preview`

--- auth.ts
+++ auth.ts
@@ -3,4 +3,7 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  // needed when using `npm run preview` and is fine for production on
+  // Cloudflare
+  trustHost: true
 });
