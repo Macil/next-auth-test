6831c9fe7b70f14e6e0e59d333ed8543d27b9174
Chris Cowan <agentme49@gmail.com>
2025-05-27 21:16:13 -0700

    fix signout not sending correct cookies in preview+production
    workaround for https://github.com/opennextjs/opennextjs-cloudflare/issues/606

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,25 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import {
+  defineCloudflareConfig,
+  type OpenNextConfig,
+} from "@opennextjs/cloudflare/config";
 
-export default defineCloudflareConfig({
+const config = defineCloudflareConfig({
   // Uncomment to enable R2 cache,
   // It should be imported as:
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
   // See https://opennext.js.org/cloudflare/caching for more details
   // incrementalCache: r2IncrementalCache,
 });
+
+export default {
+  ...config,
+  dangerous: {
+    // workaround for https://github.com/opennextjs/opennextjs-cloudflare/issues/606
+    headersAndCookiesPriority(event) {
+      if (event.method === "POST" && event.headers["next-action"]) {
+        return "handler";
+      }
+      return "middleware";
+    },
+  },
+} satisfies OpenNextConfig;
